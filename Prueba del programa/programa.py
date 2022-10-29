465a7554bcb69e22eb1022eaa973f1fd6ae60a40
Alfonso Roldán Amador <megaalfonso84@gmail.com>
2022-10-29 13:34:55 +0200

    Prueba del programa añadido

---
+++ Prueba del programa/programa.py
@@ -0,0 +1,11 @@
+from funciones import *
+print("¡Bienvendido!\nPara acceder a la información de la base de datos, introduce los parámetros de conexión.\n")
+
+maquina=input("Introduce la ip del servidor: ")
+nombrebd=input("Introduce el nombre de la base de datos: ")
+usuario=input("Introduce el usuario: ")
+contraseña=getpass.getpass("Introduce contraseña: ")
+
+db=Conectar_BD(maquina,usuario,contraseña,nombrebd)
+Mostrar_profesores_y_asignaturas(db)
+Desconectar_BD(db)
