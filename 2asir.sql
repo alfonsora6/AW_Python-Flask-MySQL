a9294b05843dba3fdd56df5bda8873a8416cee6c
Alfonso Roldán Amador <megaalfonso84@gmail.com>
2022-10-29 21:15:47 +0200

    2asir.sql modificado

--- 2asir.sql
+++ 2asir.sql
@@ -2,7 +2,7 @@
 
 --Creación de BD y usuario (Definiendole los privilegios)
 
-CREATE DATABASE alumnos;
+CREATE DATABASE 2asir;
 CREATE USER 'alfonso'@'%' IDENTIFIED BY 'passbd';
 GRANT ALL PRIVILEGES ON alumnos.* to 'alfonso'@'%';
 FLUSH PRIVILEGES;
